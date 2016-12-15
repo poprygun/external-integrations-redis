696830a6c94cb3f6945fc7297dfbd9ad202c9886
Alex Shumilov <ashumilov@pivotal.io>
2016-12-15 09:06:20 -0500

    initial set

--- src/main/java/io/pivotal/pal/LoginServlet.java
+++ src/main/java/io/pivotal/pal/LoginServlet.java
@@ -21,23 +21,21 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
 
-        // get request parameters for userID and password
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
-        if(userID.equals(user) && password.equals(pwd)){
+        if (userID.equals(user) && password.equals(pwd)) {
             HttpSession session = request.getSession();
-            session.setAttribute("user", "Pankaj");
-            //setting session to expiry in 30 mins
-            session.setMaxInactiveInterval(30*60);
+            session.setAttribute("user", "testuser");
+            session.setMaxInactiveInterval(30 * 60);
             Cookie userName = new Cookie("user", user);
-            userName.setMaxAge(30*60);
+            userName.setMaxAge(30 * 60);
             response.addCookie(userName);
             response.sendRedirect("LoginSuccess.jsp");
-        }else{
+        } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out= response.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.</font>");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Wrong username or password.</font>");
             rd.include(request, response);
         }
 
